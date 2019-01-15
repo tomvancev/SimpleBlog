e6a787d8c85675238e7553fdf1c8c46d5eb69594
Tom Vancev <tomvancev@gmail.com>
2019-01-15 15:01:51 +0100

    Basic routing

---
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController :Controller
+    {
+        public ActionResult Index()
+        {
+
+            return Content("Users");
+        }
+    }
+}
