0e1a52846d283441a0722bb07b28ad39a123bbe5
Tom Vancev <tomvancev@gmail.com>
2019-01-16 09:38:26 +0100

    Authorization

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
+
         public ActionResult Login() => View();
 
         [HttpPost]
-        public ActionResult Login(AuthLogin form){
-            if(!ModelState.IsValid)
-                 return View(form);
-            if(form.Username != "rainbow dash")
-            {
-                ModelState.AddModelError("Username", "Username invalid");
+        public ActionResult Login(AuthLogin form, string returnUrl)
+        {
+            if (!ModelState.IsValid)
                 return View(form);
+
+            FormsAuthentication.SetAuthCookie(form.Username, true);
+
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+            {
+                return Redirect(returnUrl);
             }
 
-            return Content("The Form is Valid");
+            return RedirectToRoute("home");
         }
+
+
+
     }
 }
