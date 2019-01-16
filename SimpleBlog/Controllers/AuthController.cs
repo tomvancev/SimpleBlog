1ba5d5dd33c7d641d0c7df8131abf1c275550e79
Tom Vancev <tomvancev@gmail.com>
2019-01-16 08:37:18 +0100

    View Models

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
         public ActionResult Login() => View();
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form){
+            if(!ModelState.IsValid)
+                 return View(form);
+            if(form.Username != "rainbow dash")
+            {
+                ModelState.AddModelError("Username", "Username invalid");
+                return View(form);
+            }
+
+            return Content("The Form is Valid");
+        }
     }
 }
