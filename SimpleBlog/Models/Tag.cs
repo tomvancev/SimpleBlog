07548507bdd1208d8b2303fd11e76c2336ce6393
Tom Vancev <tomvancev@gmail.com>
2019-01-18 16:29:57 +0100

    tags 1/2

---
+++ SimpleBlog/Models/Tag.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models
+{
+    public class Tag
+    {
+        public virtual int Id { get; set; }
+        public virtual string Slug { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+
+    public class TagMap : ClassMapping<Tag>
+    {
+        public TagMap()
+        {
+            Table("tags");
+
+            Id(x => x.Id, x => x.Generator((Generators.Identity)));
+
+            Property(x => x.Name, x => x.NotNullable(true));
+            Property(x => x.Slug, x => x.NotNullable(true));
+
+        }
+    }
+}
