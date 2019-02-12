f662247dec5e5e125538c1a511fb01c48093b747
Tom Vancev <tomvancev@gmail.com>
2019-02-12 16:29:36 +0100

    halway of lesson

--- SimpleBlog/App_Start/BundleConfig.cs
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -25,6 +25,14 @@ namespace SimpleBlog.App_Start
                 .Include("~/scripts/jquery.bootstrap.js")
                 .Include("~/areas/admin/scripts/forms.js"));
 
+             bundles.Add(new ScriptBundle("~/admin/scripts/post/scripts")
+                .Include("~/scripts/jquery-3.3.1.js")
+                .Include("~/scripts/jquery.validate.min.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/jquery.bootstrap.js")
+                .Include("~/areas/admin/scripts/forms.js")
+                .Include("~/areas/admin/scripts/posteditor.js"));
+
             bundles.Add(new ScriptBundle("~/scripts")
                 .Include("~/scripts/jquery-3.3.1.js")
                 .Include("~/scripts/jquery.validate.min.js")
---
+++ SimpleBlog/Areas/Admin/Scripts/PostEditor.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    var $tagEditor = $(".post-tag-editor");
+
+    $tagEditor
+        .find(".tag-select")
+        .on("click", "> li > a", function (e) {
+            e.preventDefault();
+
+            var $this = $(this);
+            var $tagParent = $this.closest("li");
+
+            $tagParent.toggleClass("selected");
+
+            var selected = $tagParent.hasClass("selected");
+            $tagParent.find(".selected-input").val(selected);
+        });
+});
